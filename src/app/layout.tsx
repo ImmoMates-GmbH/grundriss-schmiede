b7c144f68410b767e4bd7ca94f05240987a2c032
tarek339 <tarekjassine@gmail.com>
2025-05-21 09:40:30 +0200

    refactor: update CSS variables and remove unused styles; add Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
   return (
     <html lang="de">
       <body
-        className={`dark ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
