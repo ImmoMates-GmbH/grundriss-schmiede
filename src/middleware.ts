014ead012896d843b0fe9489e9292fad668af743
tarek339 <tarekjassine@gmail.com>
2025-05-21 08:29:56 +0200

    remove debug log from middleware function

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,7 +23,6 @@ function getLocale(request: NextRequest) {
 export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl;
-  console.log(pathname);
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
   );
