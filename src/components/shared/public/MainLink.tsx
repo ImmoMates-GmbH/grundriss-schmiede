d1bbbe99753103167e60e1e230390bd29fe39fa1
tarek339 <tarekjassine@gmail.com>
2025-05-23 15:37:56 +0200

    feat: add Intro component for floor plan section with image and headings

--- src/components/shared/public/MainLink.tsx
+++ src/components/shared/public/MainLink.tsx
@@ -5,7 +5,7 @@ const MainLink = ({ href, title }: MainLinkProps) => {
   return (
     <Link
       href={href}
-      className="bg-primary text-text-secondary border-primary lg:hover:bg-background lg:hover:text-primary border-2 px-12 py-2 font-semibold tracking-wider shadow-xs transition-all duration-300 *:h-9"
+      className="bg-primary text-text-secondary border-primary lg:hover:bg-background lg:hover:text-primary border-2 px-13 py-3 font-semibold tracking-wider shadow-xs transition-all duration-300 *:h-9"
     >
       {title}
     </Link>
