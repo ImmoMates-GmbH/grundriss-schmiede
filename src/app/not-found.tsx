22c4e9ac53b4c3e73a97f17786e2b40f902c179d
tarek339 <tarekjassine@gmail.com>
2025-05-23 11:09:48 +0200

    feat: add translations for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
-import Link from "next/link";
+import TitleH3 from "@/components/shared/fonts/TitleH3";
+import MainLink from "@/components/shared/public/MainLink";
+import { useTranslations } from "next-intl";
 
 export default function NotFound() {
+  const t = useTranslations("notFound");
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex h-screen flex-col items-center justify-center gap-3">
+      <TitleH3 title={t("title")} />
+      <p className="text-lg">{t("description")}</p>
+      <MainLink href={"/"} title={t("backToHome")} />
     </div>
   );
 }
